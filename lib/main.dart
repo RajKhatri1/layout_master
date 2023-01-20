b90a58390c68f3e9ade0e700062217e0c0f73569
HP <rajkhatri23066@gmail.com>
2023-01-20 10:06:22 +0530

    screen 11

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:layout1/Screen2.dart';
 import 'package:layout1/homePage.dart';
 import 'package:layout1/screen10.dart';
+import 'package:layout1/screen11.dart';
 import 'package:layout1/screen3.dart';
 import 'package:layout1/screen4.dart';
 import 'package:layout1/screen5.dart';
@@ -13,7 +14,7 @@ import 'package:layout1/screen9.dart';
 void main() {
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false,
-      home: S9(),
+      home: S11(),
     ),
   );
 }
